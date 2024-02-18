f7ddcc988c93f00bd8fff00b7a41d1176e7e077f
rtk-rnjn <ritik0ranjan@gmail.com>
2024-02-18 20:16:09 +0530

    Update footer and base templates, add mcq.html template

--- src/routes/meta.py
+++ src/routes/meta.py
@@ -1,7 +1,7 @@
 from flask import render_template
 
 from src import app
-
+import json
 
 @app.route("/")
 def index() -> str:
@@ -11,3 +11,11 @@ def index() -> str:
 @app.route("/resources/universal_ide")
 def universal_ide() -> str:
     return render_template(template_name_or_list="ide.html", title="IDE")
+
+
+with open("python.json") as f:
+    questions = json.load(f)
+
+@app.route("/test")
+def test() -> str:
+    return render_template("mcq.html", title="Test", questions=questions)
