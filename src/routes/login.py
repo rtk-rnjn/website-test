98130bf608cb8843e4f147dafdf3d2a942510012
rtk-rnjn <ritik0ranjan@gmail.com>
2024-02-20 01:15:57 +0530

    Refactor login route to use password hashing

--- src/routes/login.py
+++ src/routes/login.py
@@ -2,12 +2,11 @@ from __future__ import annotations
 
 from flask import redirect, render_template, url_for
 from flask_login import current_user, login_required, login_user
+from werkzeug.security import check_password_hash
 
 from src import app
 from src.forms import LoginForm
 from src.models import User
-
-users: dict[str, dict[str, str]] = {"email@domain.com": {"password": "password"}}
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -15,7 +14,13 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         email = form.email.data
-        if form.password.data == users[email]["password"]:
+        db = app.mongo["usersDatabase"]
+        entity = db.users.find_one({"email": email})
+        if entity is None:
+            return "<h1>Invalid email or password</h1>"
+
+        password_hash = entity["password_hash"]
+        if check_password_hash(password_hash, form.password.data):
             user = User()
             user.email = email
             login_user(user)
