d3185421315294683edaf39071e025e66585a2bd
rtk-rnjn <ritik0ranjan@gmail.com>
2024-02-21 09:20:31 +0530

    Update tio_api.py and ide.html files

--- src/routes/tio_api.py
+++ src/routes/tio_api.py
@@ -1,6 +1,6 @@
 from flask import request
 
-from src import app
+from src import app, csrf
 from src.utils.tio import Tio, parse_output
 
 
@@ -9,10 +9,14 @@ async def tio():
     lang: str = request.get_json().get("language", "")
     code: str = request.get_json().get("code", "")
 
+    print(lang, code)
     if not lang or not code:
-        return "Invalid request", 400
+        return "Invalid request", 401
 
     tio = Tio(lang, code)
     st: str = await tio.send()
 
     return {"language": lang, "code": code, **parse_output(st)}
+
+
+csrf.exempt(tio)
