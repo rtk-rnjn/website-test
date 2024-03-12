d3df4e08118b1ab3bc6c64c795e0e694d16827a1
rtk-rnjn <ritik0ranjan@gmail.com>
2024-03-12 11:37:45 +0530

    fix admin_page

--- src/routes/admin_page.py
+++ src/routes/admin_page.py
@@ -1,9 +1,11 @@
+import json
+
 import pymongo
 from bson import ObjectId
 from flask import render_template, request
 from flask_login import current_user, login_required
 
-from src import app
+from src import app, csrf
 
 mongo_client = app.mongo
 databases = mongo_client.list_database_names()
@@ -28,15 +30,18 @@ async def admin_page() -> str:
         "admin_page.html", user=current_user, cached_questions=cached_questions
     )
 
+
 cached_count = {}
+
+
 @app.route("/admin-page/<database>/<collection>")
 @login_required
 async def admin_page_db_col(database: str, collection: str) -> str:
     col = mongo_client[database][collection]
     skip = request.args.get("skip", 0)
     limit = request.args.get("limit", 20)
-    documents = (
-        col.find({}, limit=int(limit), skip=int(skip), sort=[("quesions", pymongo.ASCENDING)])
+    documents = col.find(
+        {}, limit=int(limit), skip=int(skip), sort=[("quesions", pymongo.ASCENDING)]
     )
 
     total_docs = cached_count.get(f"{database}-{collection}", col.count_documents({}))
@@ -54,7 +59,13 @@ async def admin_page_db_col(database: str, collection: str) -> str:
     )
 
 
-@app.route("/admin-page/<database>/<collection>/<question_id>/delete", methods=["DELETE", 'GET'])
+csrf.exempt(admin_page_db_col)
+
+
+@app.route(
+    "/admin-page/<database>/<collection>/<question_id>/delete",
+    methods=["DELETE", "GET"],
+)
 @login_required
 async def admin_page_db_col_delete(
     database: str, collection: str, question_id: str
@@ -62,15 +73,49 @@ async def admin_page_db_col_delete(
     col = mongo_client[database][collection]
     if request.method == "GET":
         document = col.find_one({"_id": ObjectId(question_id)}, {"_id": 0})
-        return document
+
+        return (
+            "Method GET not allowed\n" + f"<pre>{json.dumps(document, indent=4)}</pre>"
+        )
+
+    if request.method == "DELETE":
+        print("Deleted")
+
     # col.delete_one({"_id": ObjectId(question_id)})
-    return "Deleted"
+    return
 
 
-@app.route("/admin-page/<database>/<collection>/<question_id>/update")
+@app.route(
+    "/admin-page/<database>/<collection>/<question_id>/update", methods=["GET", "PATCH"]
+)
 @login_required
 async def admin_page_db_col_update(
     database: str, collection: str, question_id: str
 ) -> str:
     col = mongo_client[database][collection]
-    pass
+    if request.method == "GET":
+        document = col.find_one({"_id": ObjectId(question_id)}, {"_id": 0})
+
+        return (
+            "Method GET not allowed\n" + f"<pre>{json.dumps(document, indent=4)}</pre>"
+        )
+
+    if request.method == "PATCH":
+        json_object = request.data
+        if json_object is None:
+            return
+        json_data = json.loads(json_object)
+        print(json_data)
+
+        payload = {
+            "questions": json_data["question"],
+            "options": json_data["options"],
+            "answer": json_data["answer"],
+        }
+
+        print(payload)
+        # col.update_one({"_id": question_id}, {"$set": payload})
+    return
+
+
+csrf.exempt(admin_page_db_col_update)
