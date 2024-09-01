115915d944167288fbb6a580e9873ee5ca21d543
rtk-rnjn <ritik0ranjan+github@gmail.com>
2024-09-01 18:32:20 +0530

    chore: Add new routes for sections and update templates

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -3,3 +3,4 @@ from .logout import *  # noqa: F403
 from .landing import *  # noqa: F403
 from .register import *  # noqa: F403
 from .tio_api import *  # noqa: F403
+from .sections import *  # noqa: F403
---
+++ src/routes/sections.py
@@ -0,0 +1,31 @@
+from flask import render_template
+from flask_login import current_user
+
+from src import app
+from src.utils.caching import cache_function_result
+
+
+@cache_function_result
+async def get_topics(databse: str) -> list[str]:
+    db = app.mongo[databse]
+    collections = db.list_collection_names()
+
+    return [str(collection).replace("_", " ").capitalize() for collection in collections]
+
+
+@app.route("/competitive-reasoning", methods=["GET"])
+async def competitive_reasoning():
+    topics = await get_topics("competitive_reasoning")
+    return render_template("section.html", title="Competitive Reasoning", current_user=current_user, topics=topics)
+
+
+@app.route("/competitive-english", methods=["GET"])
+async def competitive_english():
+    topics = await get_topics("competitive_english")
+    return render_template("section.html", title="Competitive English", current_user=current_user, topics=topics)
+
+
+@app.route("/arithmetic-ability", methods=["GET"])
+async def arithmetic_ability():
+    topics = await get_topics("arithmetic_ability")
+    return render_template("section.html", title="Arithmetic Ability", current_user=current_user, topics=topics)
